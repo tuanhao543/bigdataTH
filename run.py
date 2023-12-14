24ac101ff73587ae5442be7e3b6cf67187a4d844
tuanhao543 <125726528+tuanhao543@users.noreply.github.com>
2023-12-14 20:53:56 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,16 @@
+from vega_datasets import data
+import streamlit as st 
+import altair as alt
+def main():
+    df = load_data()
+    page = st.sidebar.selectbox("Choose a page", ["Homepage", "Exploration"])
+
+    if page == "Homepage":
+        st.header("This is your data explorer.")
+        st.write("Please select a page on the left.")
+        st.write(df)
+    elif page == "Exploration":
+        st.title("Data Exploration")
+        x_axis = st.selectbox("Choose a variable for the x-axis", df.columns, index=3)
+        y_axis = st.selectbox("Choose a variable for the y-axis", df.columns, index=4)
+        visualize_data(df, x_axis, y_axis)
